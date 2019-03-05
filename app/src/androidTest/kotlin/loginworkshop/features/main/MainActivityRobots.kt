f09404b246fcdbdb8015a38c218e55cd2c664c39
André Fernandes <andrefernandes.sales@gmail.com>
2019-03-05 10:54:13 -0300

    Creating Robot pattern with arrange, act and assert for tests pattern.

---
+++ app/src/androidTest/kotlin/loginworkshop/features/main/MainActivityRobots.kt
@@ -0,0 +1,85 @@
+package loginworkshop.features.main
+
+import androidx.test.rule.ActivityTestRule
+import br.com.andrefernandesales.loginworkshop.R
+import br.com.andrefernandesales.loginworkshop.features.detail.DetailActivity
+import br.com.andrefernandesales.loginworkshop.features.main.MainActivity
+import br.com.concretesolutions.kappuccino.actions.ClickActions.click
+import br.com.concretesolutions.kappuccino.actions.TextActions.typeText
+import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.displayed
+import br.com.concretesolutions.kappuccino.custom.intent.IntentMatcherInteractions.sentIntent
+
+fun arrange(rule: ActivityTestRule<MainActivity>, func: MainActivityArrangeRobot.() -> Unit) = MainActivityArrangeRobot(rule).apply { func() }
+fun act(func: MainActivityActRobot.() -> Unit) = MainActivityActRobot().apply { func() }
+fun assert(func: MainActivityAssertRobot.() -> Unit) = MainActivityAssertRobot().apply { func() }
+
+class MainActivityArrangeRobot(private val activityRule: ActivityTestRule<MainActivity>) {
+
+    fun startActivity(){
+        activityRule.launchActivity(null)
+    }
+}
+
+class MainActivityActRobot {
+
+    fun clickLoginButton() {
+        click {
+            id(R.id.main_btn_action)
+        }
+    }
+
+    fun typePassword(password: String) {
+        typeText(password, false, false, true) {
+            id(R.id.main_ed_password)
+        }
+    }
+
+    fun typeUsername(username: String) {
+        typeText(username, false, false, true) {
+            id(R.id.main_ed_login)
+        }
+    }
+
+}
+
+class MainActivityAssertRobot {
+
+    fun title() {
+        displayed {
+            id(R.id.main_txt_title)
+        }
+    }
+
+    fun password() {
+        displayed {
+            id(R.id.main_ed_password)
+        }
+    }
+
+    fun username() {
+        displayed {
+            id(R.id.main_ed_login)
+        }
+    }
+
+    fun actionButton() {
+        displayed {
+            id(R.id.main_btn_action)
+        }
+    }
+
+    fun snackbar(message: Int) {
+        displayed {
+            allOf {
+                id(com.google.android.material.R.id.snackbar_text)
+                text(message)
+            }
+        }
+    }
+
+    fun redirectToDetail() {
+        sentIntent {
+            className(DetailActivity::class.java.name)
+        }
+    }
+}
---
+++ app/src/androidTest/kotlin/loginworkshop/features/main/MainActivityTest.kt
@@ -0,0 +1,116 @@
+package loginworkshop.features.main
+
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.filters.LargeTest
+import br.com.andrefernandesales.loginworkshop.R
+import br.com.andrefernandesales.loginworkshop.features.main.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = IntentsTestRule<MainActivity>(MainActivity::class.java, false, false)
+
+    @Test
+    fun whenLoadActivity_ShouldShowTitle() {
+        arrange(activityRule) {
+            startActivity()
+        }
+
+        assert {
+            title()
+        }
+    }
+
+    @Test
+    fun whenLoadActivity_UsernameFieldAndPasswordShouldBeVisible() {
+        arrange(activityRule) {
+            startActivity()
+        }
+
+        assert {
+            username()
+            password()
+        }
+    }
+
+    @Test
+    fun whenLoadActivity_ButtonShouldBeVisible() {
+        arrange(activityRule) {
+            startActivity()
+        }
+
+        assert {
+            actionButton()
+        }
+    }
+
+    @Test
+    fun whenHasUserName_ClickedToLogin_ShouldShowCredentialsInvalid() {
+        arrange(activityRule) {
+            startActivity()
+        }
+
+        act {
+            typeUsername("Teste")
+            clickLoginButton()
+        }
+
+        assert { snackbar(R.string.main_fill_error_message) }
+    }
+
+    @Test
+    fun whenHasPasswordTyped_ClickedToLogin_ShouldShowCredentialsInvalid() {
+        arrange(activityRule) {
+            startActivity()
+        }
+
+        act {
+            typePassword("1234567")
+            clickLoginButton()
+        }
+
+        assert {
+            snackbar(R.string.main_fill_error_message)
+        }
+    }
+
+    @Test
+    fun whenHasUserNameAndWrongPassword_ClickedToLogin_ShouldShowPasswordInvalid() {
+        arrange(activityRule) {
+            startActivity()
+        }
+
+        act {
+            typeUsername("Teste")
+            typePassword("1234567")
+            clickLoginButton()
+        }
+
+        assert {
+            snackbar(R.string.main_password_error_message)
+        }
+    }
+
+    @Test
+    fun whenHasUserAndPasswordCorrect_ClickedToLogin_ShouldRedirectIntoDetailActivity() {
+        arrange(activityRule) {
+            startActivity()
+        }
+
+        act {
+            typeUsername("Teste")
+            typePassword("12345678")
+            clickLoginButton()
+        }
+
+        assert {
+            redirectToDetail()
+        }
+    }
+}
